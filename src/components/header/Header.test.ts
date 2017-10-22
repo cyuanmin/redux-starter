a63844c0bf62fb6110bfa25d3b6a02bb92ab378b
Yuanming Chen <yuanming@hotmail.com>
2017-10-22 00:08:22 -0600

    Added deep copy test. Have to change tsconfig to support ES2015

--- src/components/header/Header.test.ts
+++ src/components/header/Header.test.ts
@@ -6,4 +6,16 @@ describe("Test Header", function () {
         const a: number = 3;
         expect(a).to.eq(1 + 2);
     });
+
+    it("Object assign", function () {
+        const state: any = {
+            role: "admin",
+            age: 44
+        };
+
+        const changedState = Object.assign({}, state, {role: "client"});
+        expect(state).to.not.eq(changedState);
+        expect(changedState.age).to.eq(44);  
+        expect(changedState.role).to.eq("client");  
+    });
 });
